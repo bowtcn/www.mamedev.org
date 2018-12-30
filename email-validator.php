de73d9ce00dca101573f0ddf1cbc44915fe0465a
joyist2018 <35645916+joyist2018@users.noreply.github.com>
2018-12-30 16:30:09 +0800

    Update email-validator.php

--- email-validator.php
+++ email-validator.php
@@ -2,17 +2,17 @@
 
     /*
 
-        EmailAddressValidator Class
+        电子邮件地址验证器类 Email Address Validator Class
         http://code.google.com/p/php-email-address-validation/
 
-        Released under New BSD license
+        根据新BSD许可证发布
         http://www.opensource.org/licenses/bsd-license.php
         
-        Sample Code
+        示例代码
         ----------------
         $validator = new EmailAddressValidator;
         if ($validator->check_email_address('test@example.org')) {
-            // Email address is technically valid
+            // 电子邮件地址在技术上有效
         }
 
     */
@@ -20,43 +20,43 @@
     class EmailAddressValidator {
 
         /**
-         * Check email address validity
-         * @param   strEmailAddress     Email address to be checked
-         * @return  True if email is valid, false if not
+         * 检查邮件地址有效性
+         * @param   strEmailAddress     要检查的电子邮件地址
+         * @return  如果电子邮件有效则为True，否则为false
          */
         public function check_email_address($strEmailAddress) {
             
-            // If magic quotes is "on", email addresses with quote marks will
-            // fail validation because of added escape characters. Uncommenting
-            // the next three lines will allow for this issue.
+            //如果魔术引号为“on”，带引号的电子邮件地址将会出现
+            //因为添加了转义字符而无法验证。取消注释
+            //接下来的三行将允许此问题。
             //if (get_magic_quotes_gpc()) { 
             //    $strEmailAddress = stripslashes($strEmailAddress); 
             //}
 
-            // Control characters are not allowed
+            // 不允许使用控制字符
             if (preg_match('/[\x00-\x1F\x7F-\xFF]/', $strEmailAddress)) {
                 return false;
             }
 
-            // Check email length - min 3 (a@a), max 256
+            // 检查电子邮件长度 - 最小3（a @ a），最大256
             if (!$this->check_text_length($strEmailAddress, 3, 256)) {
                 return false;
             }
 
-            // Split it into sections using last instance of "@"
+            // 使用“@”的最后一个实例将其拆分为多个部分
             $intAtSymbol = strrpos($strEmailAddress, '@');
             if ($intAtSymbol === false) {
-                // No "@" symbol in email.
+                // 电子邮件中没有“@”符号
                 return false;
             }
             $arrEmailAddress[0] = substr($strEmailAddress, 0, $intAtSymbol);
             $arrEmailAddress[1] = substr($strEmailAddress, $intAtSymbol + 1);
 
-            // Count the "@" symbols. Only one is allowed, except where 
-            // contained in quote marks in the local part. Quickest way to
-            // check this is to remove anything in quotes. We also remove
-            // characters escaped with backslash, and the backslash
-            // character.
+            //计算“@”符号。除了在哪里，只允许一个
+            //包含在本地部分的引号中。最快捷的方式
+            //检查这是删除引号中的任何内容。我们也删除了
+            //使用反斜杠转义的字符和反斜杠
+            //字符
             $arrTempAddress[0] = preg_replace('/\./'
                                              ,''
                                              ,$arrEmailAddress[0]);
@@ -65,43 +65,43 @@
                                              ,$arrTempAddress[0]);
             $arrTempAddress[1] = $arrEmailAddress[1];
             $strTempAddress = $arrTempAddress[0] . $arrTempAddress[1];
-            // Then check - should be no "@" symbols.
+            // 然后检查 - 应该没有“@”符号。
             if (strrpos($strTempAddress, '@') !== false) {
                 // "@" symbol found
                 return false;
             }
 
-            // Check local portion
+            // 检查本地部分
             if (!$this->check_local_portion($arrEmailAddress[0])) {
                 return false;
             }
 
-            // Check domain portion
+            // 检查域名部分
             if (!$this->check_domain_portion($arrEmailAddress[1])) {
                 return false;
             }
 
-            // If we're still here, all checks above passed. Email is valid.
+            // 如果我们还在这里，上面的所有检查都会通过。电子邮件有效。
             return true;
 
         }
 
-        /**
-         * Checks email section before "@" symbol for validity
-         * @param   strLocalPortion     Text to be checked
-         * @return  True if local portion is valid, false if not
-         */
+        / **
+         *检查“@”符号前的电子邮件部分是否有效
+         * @param    strLocalPortion要检查的文本
+         * @return   如果本地部分有效则为True，否则为false
+         * /
         protected function check_local_portion($strLocalPortion) {
-            // Local portion can only be from 1 to 64 characters, inclusive.
-            // Please note that servers are encouraged to accept longer local
-            // parts than 64 characters.
+            //本地部分只能包含1到64个字符。
+            //请注意，鼓励服务器接受更长的本地服务
+            //超过64个字符的部分。
             if (!$this->check_text_length($strLocalPortion, 1, 64)) {
                 return false;
             }
-            // Local portion must be:
-            // 1) a dot-atom (strings separated by periods)
-            // 2) a quoted string
-            // 3) an obsolete format string (combination of the above)
+            //本地部分必须是：
+            // 1）点原子（由句点分隔的字符串）
+            // 2）带引号的字符串
+            // 3）过时的格式字符串（以上组合）
             $arrLocalPortion = explode('.', $strLocalPortion);
             for ($i = 0, $max = sizeof($arrLocalPortion); $i < $max; $i++) {
                  if (!preg_match('.^('
