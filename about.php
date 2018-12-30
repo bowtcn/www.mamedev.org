dee2a953c039c19c8575e7befca1887f0b06ecd5
joyist2018 <35645916+joyist2018@users.noreply.github.com>
2018-12-30 16:04:36 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,68 +1,68 @@
 <?php
-$title = 'MAME | About MAME';
+$title = 'MAME | 关于';
 ?>
 	<?php require($_SERVER['DOCUMENT_ROOT'] . '/_include/html/header.html'); ?>
 
-    <!-- Page Content -->
+    <!-- 页面内容 Page Content -->
     <div class="container">
 
-		<center><h1 class="page-header">About MAME</h1></center>
+		<center><h1 class="page-header">关于MAME</h1></center>
 		
 
-		<h2>Purpose</h2>
+		<h2>目的|Purpose</h2>
                     <p>
-MAME's main purpose is to be a reference to the inner workings of the
-emulated machines. This is done both for educational purposes and for
-preservation purposes, in order to prevent historical software from
-disappearing forever once the hardware it runs on stops working. Of
-course, in order to preserve the software and demonstrate that the 
-emulated behavior matches the original, you must also be able to
-actually use the software. This is considered a nice side effect, and is
-not MAME's primary focus. 
+MAME的主要目的是成为对内部运作的参考
+仿真机器。这既是出于教育目的，也是出于教育目的
+保存目的，以防止历史软件
+一旦运行的硬件停止工作，它将永远消失。的
+当然，为了保存软件并证明了这一点 
+模拟行为与原始行为相匹配，您也必须能够
+实际上使用该软件。这被认为是一个很好的副作用，而且是
+不是MAME的主要关注点。  
                     </p>
                     <p>
-It is not our intention to infringe on any copyrights or patents on the
-original games. All of MAME's source code is either our own or freely
-available. To operate, the emulator requires images of the original
-ROMs, CDs, hard disks or other media from the machines, which must be
-provided by the user. No portions of the original game code are included
-in the executable.
+我们无意侵犯任何版权或专利
+原创游戏。MAME的所有源代码都是我们自己的或自由的
+可用。要进行操作，仿真器需要原始图像
+来自机器的ROM，CD，硬盘或其他媒体，必须是
+由用户提供。不包括原始游戏代码的任何部分
+在可执行文件中。
                     </p>
                  
-                 	<h2>Cost</h2>
+                 	<h2>费用|Cost</h2>
                     <p>
-MAME is free. Its source code is free. The project as whole is under
-distributed under the GNU General Public License, version 2 (GPL-2.0),
-but most of code (including core functionality) is available under the
-BSD-3-clause license.
+MAME是免费的。它的源代码是免费的。整个项目正在进行中
+根据GNU通用公共许可证第2版（GPL-2.0）分发，
+但大多数代码（包括核心功能）都可以在
+BSD-3条款许可。
                     </p>
-                 	<h2>Software Image Files</h2>
+                 	<h2>软件映像文件|Software Image Files</h2>
                     <p>
-ROM, CD, hard disk and other media images are all copyrighted material.
-They cannot be distributed without the explicit permission of the
-copyright holder(s). They are not "abandonware", nor has any of the
-software supported by MAME passed out of copyright.
+ROM，CD，硬盘和其他媒体图像都是受版权保护的材料。
+未经明确许可，不得分发
+版权所有者。它们不是“放弃软件”，也不是“放弃软件”
+MAME支持的软件已通过版权保护。
                     </p>
                     <p>
-MAME is not intended to be used as a tool for mass copyright
-infringement. Therefore, it is strongly against the authors' wishes to
-sell, advertise, or link to resources that provide illegal copies of
-ROM, CD, hard disk or other media images.
+MAME不能用作大规模版权的工具
+侵权。因此，它强烈反对提交人的意愿
+出售，宣传或链接提供非法副本的资源
+ROM，CD，硬盘或其他媒体图像。
                     </p>
-                 	<h2>Derivative Works</h2>
+                 	<h2>衍生作品|Derivative Works</h2>
                     <p>
-Because the name MAME is trademarked, you must abide by the rules set
-out for trademark usage if you wish to use "MAME" as part of the name
-your derivative work. In general, this means you must request
-permission, which requires that you follow the guidelines above.
+由于MAME的名称是商标，因此您必须遵守规则集
+如果您希望使用“MAME”作为名称的一部分，请注明商标用法
+你的衍生作品。一般来说，这意味着您必须要求
+许可，要求您遵循上述指南。
                     </p>
-					<p>The version number of any derivative work should reflect the version
-number of the MAME release from which it was was derived.
+					<p>任何衍生作品的版本号应反映版本
+导出的MAME版本号。
 
 					</p>
-                 	<h2>Official Contact Information</h2>
-                    <p>For questions regarding the MAME license, trademark, or other usage, go to http://mamedev.org/.</p>
-                    <p>MAME is a registered trademark of Gregory Ember. </p>
+                 	<h2>官方联系信息|Official Contact Information</h2>
+                    <p>有关MAME许可，商标或其他用法的问题，请访问http://mamedev.org/.</p>
+                    <p>MAME是Gregory Ember的注册商标。</p>
 <br/><br/>                         
 	</div>
     <!-- /.container -->
